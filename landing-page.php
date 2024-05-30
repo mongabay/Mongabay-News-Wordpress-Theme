4e261f7033388898a6a33396c533075741ea5920
Martin Krasilnikov <martin.krasilnikov@bynder.com>
2024-05-30 09:11:17 +0200

    landing page initial setup

---
+++ landing-page.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * Template Name: Landing Page
+ *
+ * @package WordPress
+ * @subpackage Mongabay_v2
+ * @since Mongabay 1.0
+ */
+get_header(); ?>
+
+
+<div class="container gap--40">
+	<!-- Component Start -->
+	<div class="column--20 latest-news">
+		<div class="section-title gap--8">
+			<h4>Latest</h4>
+			<div class="divider"></div>
+		</div>
+		<div class="article--container ph--8">
+			<div class="featured-image">
+				<img src="./img/01.jpg" />
+			</div>
+			<div class="title headline">
+				<h3>Green credentials of electric vehicles come under fire</h3>
+			</div>
+			<div class="meta">
+				<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+				<span class="date">23 May 2024</span>
+			</div>
+		</div>
+		<div class="article--container ph--8">
+			<div class="title headline">
+				<h3>Green credentials of electric vehicles come under fire</h3>
+			</div>
+			<div class="meta">
+				<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+				<span class="date">23 May 2024</span>
+			</div>
+		</div>
+		<div class="article--container ph--8">
+			<div class="title headline">
+				<h3>Green credentials of electric vehicles come under fire</h3>
+			</div>
+			<div class="meta">
+				<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+				<span class="date">23 May 2024</span>
+			</div>
+		</div>
+		<div class="article--container ph--8">
+			<div class="title headline">
+				<h3>Green credentials of electric vehicles come under fire</h3>
+			</div>
+			<div class="meta">
+				<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+				<span class="date">23 May 2024</span>
+			</div>
+		</div>
+		<div class="article--container ph--8">
+			<div class="title headline">
+				<h3>Green credentials of electric vehicles come under fire</h3>
+			</div>
+			<div class="meta">
+				<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+				<span class="date">23 May 2024</span>
+			</div>
+		</div>
+		<a class="theme--button outlined full-width">All news <span class="icon icon-right"></span></a>
+		<!-- Component End -->
+	</div>
+	<div class="column--60 latest-featured gap--8">
+		<!-- Component Start -->
+		<div class="section-title gap--16">
+			<h4>Top stories</h4>
+			<div class="divider"></div>
+		</div>
+		<div class="article--container">
+			<div class="featured-image">
+				<img src="./img/02.jpg" />
+				<div class="title headline">
+					<h1>Green credentials of electric vehicles come under fire</h1>
+				</div>
+			</div>
+		</div>
+		<div class="grid--2 gap--40">
+			<div class="article--container ph--8">
+				<div class="featured-image">
+					<img src="./img/03.jpg" />
+				</div>
+				<div class="title headline">
+					<h3>Green credentials of electric vehicles come under fire</h3>
+				</div>
+				<div class="meta">
+					<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+					<span class="date">23 May 2024</span>
+				</div>
+			</div>
+			<div class="article--container ph--8">
+				<div class="featured-image">
+					<img src="./img/04.jpg" />
+				</div>
+				<div class="title headline">
+					<h3>Green credentials of electric vehicles come under fire</h3>
+				</div>
+				<div class="meta">
+					<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+					<span class="date">23 May 2024</span>
+				</div>
+			</div>
+			<div class="article--container ph--8">
+				<div class="featured-image">
+					<img src="./img/05.jpg" />
+				</div>
+				<div class="title headline">
+					<h3>Green credentials of electric vehicles come under fire</h3>
+				</div>
+				<div class="meta">
+					<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+					<span class="date">23 May 2024</span>
+				</div>
+			</div>
+			<div class="article--container ph--8">
+				<div class="featured-image">
+					<img src="./img/06.jpg" />
+				</div>
+				<div class="title headline">
+					<h3>Green credentials of electric vehicles come under fire</h3>
+				</div>
+				<div class="meta">
+					<span class="byline"><a href="">Gerry McGovern, Sue Branford</a></span>
+					<span class="date">23 May 2024</span>
+				</div>
+			</div>
+		</div>
+		<!-- Component End -->
+	</div>
+	<div class="column--20 gap--40 ph--40 latest-banners">
+		<!-- Component Start -->
+		<div class="banner outlined gap--20 ph--20 pv--20">
+			<div class="title">
+				<h1>Stay updated</h1>
+			</div>
+			<div class="copy">Delivering news and inspiration from nature’s frontline.</div>
+			<a href="" class="theme--button full-width primary">Newsletter <span class="icon icon-right"></span></a>
+		</div>
+		<!-- Component End -->
+		<!-- Component Start -->
+		<div class="banner accent gap--20 ph--20 pv--20">
+			<div class="title">
+				<h1>We are nonprofit</h1>
+			</div>
+			<div class="copy">
+				Help us tell stories of biodiversity loss, climate change and more.
+			</div>
+			<a href="" class="theme--button full-width primary">Donate <span class="icon icon-right"></span></a>
+		</div>
+		<!-- Component End -->
+	</div>
+</div>
+<!-- /container -->
+<?php get_footer(); ?>
