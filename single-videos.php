e5660e161c441d4c3e90beb976b35afb93ac5632
Right-Noe <157106140+Right-Noe@users.noreply.github.com>
2024-06-05 19:15:56 +0200

    Update single-videos.php
    - Remove translator/adaptor
    - Added byline thumbnail (single-article-meta)
    - Added comments number ((single-article-meta)
    - Added video input from Pods field
    - Remove donation
    - Added components after article

--- single-videos.php
+++ single-videos.php
@@ -2,14 +2,14 @@
 <main role="main">
   <?php
   $post_id = get_the_ID();
-  $translator = get_post_meta($post_id, "translated_by", true);
-  $adaptor = get_post_meta($post_id, "adapted_by", true);
-  $translated_adapted = get_post_meta($post_id, "translated_adapted", true);
   $commentary = get_post_meta($post_id, "commentary", true);
   $analysis = get_post_meta($post_id, "analysis_by", true);
   $topics = wp_get_post_terms($post_id, 'topic');
   $serial = wp_get_post_terms($post_id, 'serial');
   $legacy = get_post_meta($post_id, 'mongabay_post_legacy_status', true);
+  $img_url = wp_get_attachment_url(get_post_thumbnail_id());
+  $byline_terms = wp_get_post_terms($post_id,'byline');
+  $avatar = get_term_meta($byline_terms[0]->term_id,'cover_image_url',true);
   ?>
   <div class="container in-column ph--40">
     <div id="headline">
@@ -32,34 +32,55 @@
         ?>
       </div>
       <div class="single-article-meta">
-        <?php if ($commentary == '1' || $commentary == 'yes') _e('Commentary ', 'mongabay');
-        if ($analysis == '1' || $analysis == 'yes') _e('Analysis ', 'mongabay');
-        _e('by ', 'mongabay'); ?><?php echo get_the_term_list($post_id, 'byline', '', ', ', ''); ?><?php _e(' on ', 'mongabay'); ?><?php the_time('j F Y'); ?>
-        <?php
-        if ($translated_adapted == 'adapted' || $translated_adapted == 'translated') {
-
-          if ($translated_adapted == 'adapted' && !empty($adaptor)) {
-            $string_title = 'Adapted by ';
-            $translator_adaptor = $adaptor;
-          } elseif ($translated_adapted == 'translated' && !empty($translator)) {
-            $string_title = 'Translated by ';
-            $translator_adaptor = $translator;
+        <div class="about-author">
+          <div class="author-avatar">
+              <?php if($avatar){ echo '<img src="' . $avatar . '" alt="cover image" style="max-width:48px;height:auto;">';
+                } else { echo '<span class="meta-author-circle"></span>';
+                } ?>
+          </div>
+          <div class="author-info">
+            <?php echo get_the_term_list($post_id, 'byline', '', ', ', ''); ?><?php the_time('j F Y'); ?>
+          </div>
+        </div>
+        <?php $comments_number = get_comments_number();
+          if ( $comments_number > 1 ){
+            printf( esc_html__('%s Comments', 'mongabay'), get_comments_number_text( '0', '1', '%' ) );
           }
-          echo '| ';
-          _e($string_title, 'mongabay');
-          echo ' ';
-          echo '<a href="' . home_url('/') . 'by/' . $translator_adaptor['slug'] . '">' . $translator_adaptor['name'] . '</a>';
-        }
-        ?>
+          else {
+            esc_html_e( '1 Comment', 'mongabay' );
+          } ?>
+        <?php if ($commentary == '1' || $commentary == 'yes') _e('Commentary ', 'mongabay');
+        if ($analysis == '1' || $analysis == 'yes') _e('Analysis ', 'mongabay'); ?>        
+
         <?php
         if (!mongabay_wp_is_mobile()) {
           echo '<div class="social">';
-          // get_template_part('partials/section', 'social');
+          get_template_part('partials/section', 'social');
           echo '</div>';
         }
         ?>
       </div>
+      <div class="featured-area">
+        <?php echo pods_field_display( 'video_source' ); ?>
+      </div>
     </div>
+    <?php if (!empty($img_url)) : ?>
+      <div class="row article-cover-image no-gutters">
+        <?php
+        if (wp_is_mobile()) {
+          $coversize = 'medium';
+        } elseif (strlen(get_the_post_thumbnail_url($post_id, 'cover-image-retina')) > 0) {
+          $coversize = 'cover-image-retina';
+        } else {
+          $coversize = 'large';
+        }
+        ?>
+        <div class="col-lg-12" style="background: url('<?php echo get_the_post_thumbnail_url($post_id, $coversize) ?>');background-size: cover; background-position: center"></div>
+
+      </div>
+    <?php endif; ?>
+
+
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <!-- article -->
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -83,9 +104,6 @@
           <?php mongabay_sanitized_content($post_id); ?>
           <div id="single-article-footer">
             <div id="single-article-meta">
-              <?php if (get_current_blog_id() === 20) {
-                echo '<a href="https://mongabay.org/donate/?utm_source=mongabay.com&utm_medium=bottom-of-news-post&utm_campaign=support-our-journalism-2023" style="display:block;margin-bottom:2rem;"><img src="https://imgs.mongabay.com/wp-content/uploads/sites/20/2022/01/15035219/support-our-journalism.jpg" width="480" height="258"></a>';
-              } ?>
               <span>
                 <?php _e('Article published by ', 'mongabay'); ?><?php the_author_posts_link(); ?></span>
               <span class="article-comments"><a href=""></a></span>
@@ -112,5 +130,27 @@
       <!-- /article -->
     <?php endif; ?>
   </div>
+  <?php // series_articles_listing(); ?>
+  <div class="container centered pv--40">
+    <?php banner('', 'We are nonprofit', 'Help us tell stories of biodiversity loss, climate change and more.', 'Donate', 'accent ph--20 pv--56 full-width', 'extra-large'); ?>
+  </div>
+  <div id="section-videos-latest" class="container pv--40 gap--20 in-column">
+    <div class="container in-row full-width section--headline" style="align-items: center; justify-content: space-between;">
+      <h1><?php _e('Latest videos', 'mongabay'); ?></h1>
+      <a href="" class="theme--button primary"><?php _e('All videos', 'mongabay'); ?><span class="icon icon-right"></span></a>
+    </div>
+    <?php videos_latest(); ?>
+  </div>
+  <?php banner('', 'Stay updated', 'Delivering news and inspiration from nature’s frontline.', 'Newsletter', 'accent ph--20 pv--56 full-width', 'extra-large'); ?>
+
+  <div class="container full--width pv--40 gap--40 in-column">
+    <div class="grid--4 gap--20 repeat">
+      <?php // articles_listing('videos', 8, 0, true, 'medium', null); ?>
+    </div>
+    <div class="container centered pv--40">
+      <a href="" class="theme--button primary"><?php _e('All videos', 'mongabay'); ?><span class="icon icon-right"></span></a>
+    </div>
+  </div>
+  <?php inspiration_banner(); ?>
 </main>
 <?php get_footer(); ?>
