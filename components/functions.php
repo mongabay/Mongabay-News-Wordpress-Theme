ce9d2b9664ebc07400ecd7f867b2dc30dbb37f09
Right-Noe <157106140+Right-Noe@users.noreply.github.com>
2024-06-11 15:31:17 +0200

    Article credits include functions.php

--- components/functions.php
+++ components/functions.php
@@ -23,3 +23,4 @@ include(get_template_directory() . '/components/lib/social-share.php');
 include(get_template_directory() . '/components/lib/series-latest.php');
 include(get_template_directory() . '/components/lib/post-icon.php');
 include(get_template_directory() . '/components/lib/article-headline.php');
+include(get_template_directory() . '/components/lib/article-credits.php');
