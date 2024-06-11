618bad5c3ab1f493848780093afdaf362d008e4e
martinosmartinos <martinosmartinos@gmail.com>
2024-06-11 15:49:42 +0200

    styling for tags

--- components/lib/article-terms.php
+++ components/lib/article-terms.php
@@ -1,7 +1,12 @@
 <?php
 function article_terms(int $post_id)
 {
+  echo '<div class="section-title gap--16"><h4>';
+  _e('Topics', 'mongabay');
+  echo '</h4><div class="divider"></div></div>';
+  echo '<div class="container wrapped">';
   echo get_the_term_list($post_id, 'topic', '', '', '');
   echo get_the_term_list($post_id, 'location', '', '', '');
   echo get_the_term_list($post_id, 'entity', '', '', '');
+  echo '</div>';
 }
